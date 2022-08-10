383f594c81d395b2bcd77cdd776b37c3debd2c97
micah rich <micah@micahrich.com>
2022-08-10 13:58:44 -0400

    comment out fileConfig in to-string

--- src/generators/output/to-string.js
+++ src/generators/output/to-string.js
@@ -7,61 +7,68 @@ const posthtmlMSO = require('../../transformers/posthtmlMso')
 const Config = require('../config')
 
 module.exports = async (html, options) => {
-  process.env.NODE_ENV = get(options, 'maizzle.env', 'local')
+  process.env.NODE_ENV = get(options, "maizzle.env", "local");
 
-  if (typeof html !== 'string') {
-    throw new TypeError(`first argument must be an HTML string, received ${html}`)
+  if (typeof html !== "string") {
+    throw new TypeError(
+      `first argument must be an HTML string, received ${html}`
+    );
   }
 
   if (html.length === 0) {
-    throw new RangeError('received empty string')
+    throw new RangeError("received empty string");
   }
 
-  const fileConfig = await Config.getMerged(process.env.NODE_ENV)
+  // const fileConfig = await Config.getMerged(process.env.NODE_ENV)
 
-  let config = merge(fileConfig, get(options, 'maizzle', {}))
+  let config = get(options, "maizzle", {});
 
-  const tailwindConfig = get(options, 'tailwind.config', {})
-  const cssString = get(options, 'tailwind.css', '')
+  const tailwindConfig = get(options, "tailwind.config", {});
+  const cssString = get(options, "tailwind.css", "");
 
-  const {frontmatter} = fm(html)
+  const { frontmatter } = fm(html);
 
-  html = `---\n${frontmatter}\n---\n\n${fm(html).body}`
+  html = `---\n${frontmatter}\n---\n\n${fm(html).body}`;
 
-  config = merge({applyTransformers: true}, config, fm(html).attributes)
+  config = merge({ applyTransformers: true }, config, fm(html).attributes);
 
-  if (typeof get(options, 'tailwind.compiled') === 'string') {
-    config.css = options.tailwind.compiled
+  if (typeof get(options, "tailwind.compiled") === "string") {
+    config.css = options.tailwind.compiled;
   } else {
-    config.css = await Tailwind.compile(cssString, html, tailwindConfig, config)
+    config.css = await Tailwind.compile(
+      cssString,
+      html,
+      tailwindConfig,
+      config
+    );
   }
 
-  if (options && typeof options.beforeRender === 'function') {
-    html = await options.beforeRender(html, config)
+  if (options && typeof options.beforeRender === "function") {
+    html = await options.beforeRender(html, config);
   }
 
-  html = await posthtml(html, config)
+  html = await posthtml(html, config);
 
   while (Object.keys(fm(html).attributes).length > 0) {
-    html = fm(html).body
+    html = fm(html).body;
   }
 
-  if (options && typeof options.afterRender === 'function') {
-    html = await options.afterRender(html, config)
+  if (options && typeof options.afterRender === "function") {
+    html = await options.afterRender(html, config);
   }
 
   if (config.applyTransformers) {
-    html = await Transformers.process(html, config)
+    html = await Transformers.process(html, config);
   }
 
-  if (options && typeof options.afterTransformers === 'function') {
-    html = await options.afterTransformers(html, config)
+  if (options && typeof options.afterTransformers === "function") {
+    html = await options.afterTransformers(html, config);
   }
 
-  html = await posthtmlMSO(html, config)
+  html = await posthtmlMSO(html, config);
 
   return {
     html,
-    config
-  }
-}
+    config,
+  };
+};
